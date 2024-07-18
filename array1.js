5a22179e6e4743fab9669664686e9feea7bc635d
ShivamKakde2001 <kakdeshivam26@gmail.com>
2024-07-18 21:28:36 +0530

    Add files via upload
    Array Programe

---
+++ array1.js
@@ -0,0 +1,8 @@
+function biggest_num(){
+    let array = [2,4,5,1,8,6,3,5];
+    let sorted = array.sort();
+    console.log(sorted);
+    big_num = BigInt64Array.sorted;
+    console.log(big_num);
+}
+biggest_num();
