d896a76b60d4f74527137f7fc6109f8ba281a9b6
ShivamKakde2001 <kakdeshivam26@gmail.com>
2024-07-25 11:44:46 +0530

    Add files via upload
    Basic Type Conversion

---
+++ typeconversion.js
@@ -0,0 +1,21 @@
+// type conversion :-
+
+
+let score = "33";
+console.log(typeof score);
+
+valueNumber = Number(score);
+console.log(typeof valueNumber);
+
+let valueString = String(valueNumber);
+console.log(typeof valueString);
+
+
+// NaN :- Not a Number
+
+let isLoggedIN = true;
+console.log(typeof isLoggedIN);
+
+let isLoggedOut = Boolean(1);
+console.log(typeof isLoggedOut);
+console.log(isLoggedOut)
